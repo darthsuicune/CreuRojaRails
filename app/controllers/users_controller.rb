aa9b4aeda7224641ce726cc111889da554e15a92
Denis Lapuente <darthsuicune@gmail.com>
2014-05-01 23:32:15 +0200

    Add log hook

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,5 +1,6 @@
 class UsersController < ApplicationController
 	before_action :set_user, only: [:show, :edit, :update, :destroy]
+	after_action :log
 
 	# GET /users
 	# GET /users.json
@@ -76,5 +77,8 @@ class UsersController < ApplicationController
 	# Never trust parameters from the scary internet, only allow the white list through.
 	def user_params
 		params.require(:user).permit(:name, :surname, :email, :password, :password_confirmation, :resettoken, :resettime, :language, :role)
+	end
+	
+	def log
 	end
 end
