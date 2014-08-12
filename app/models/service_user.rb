c2491d25f5fda56031e27a66d6345c0df926c52a
Denís <lapuente@abbyy.com>
2014-08-12 17:07:06 +0200

    Add model files

---
+++ app/models/service_user.rb
@@ -0,0 +1,14 @@
+class ServiceUser < ActiveRecord::Base
+	belongs_to :user
+	belongs_to :service
+	
+	before_validation :defaults
+	
+	validates :service_id, presence: true
+	validates :user_id, presence: true
+	
+	private
+	def defaults
+		location_id ||= service.first_location_id
+	end
+end
