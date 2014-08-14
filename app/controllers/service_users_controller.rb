cf98c655c2b724433d8ae183b2a5c8540fe93bfd
Denís <lapuente@abbyy.com>
2014-08-14 17:05:16 +0200

    Fix some stuff

--- app/controllers/service_users_controller.rb
+++ app/controllers/service_users_controller.rb
@@ -1,6 +1,7 @@
 class ServiceUsersController < ApplicationController
 	before_filter :signed_in_user
 	before_filter :is_valid_user, only: [:create, :update, :destroy]
+	before_filter :is_valid_data, only: [:create, :update]
 	
 	def create
 		service_user = ServiceUser.new(service_user_params)
@@ -26,10 +27,19 @@ class ServiceUsersController < ApplicationController
 	
 	private
 	def service_user_params
-		params.require(:service_user).permit(:service_id, :user_id, :location_id, :user_position)
+		params.require(:service_user).permit(:service_id, :user_id, :location_id, :vehicle_id, :user_position)
 	end
 	
 	def is_valid_user
 		redirect_to root_url unless current_user.allowed_to?(:assign_user_to_service) || current_user?(User.find(params[:service_user][:user_id]))
+	end
+	
+	def is_valid_data
+		service_user = ServiceUser.new(service_user_params)
+		if (service_user.location_id == -1 && service_user.vehicle_id != -1) || (service_user.location_id != -1 && service_user.vehicle_id == -1)
+			true
+		else
+			redirect_to service_user.service, notice: I18n.t(:form_add_user_to_service_warning_only_location_or_vehicle)
+		end
 	end
 end
